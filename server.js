344f7fb9dd881a8d9ba6a4fc2fd75cacb8f61012
dhruv <dr26dhruv@gmail.com>
2024-05-13 18:51:53 +0530

    dotenv file updation

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
+import dotenv from "dotenv";
 import { ApolloServer,gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import typeDefs from './SchemaGQL.js'
-import resolvers from './resolvers.js'
+import mongoose from 'mongoose'
 
+dotenv.config({
+  path: "./.env",
+});
+
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+})
+
+
+mongoose.connection.on("connected",()=>{
+  console.log("Connected to mongodb");
+})
+
+mongoose.connection.on("error", (err) => {
+  console.log("Failed to connect to mongodb",err );
+});
+
+//import models here 
+import "./models/user.js"
+import "./models/quotes.js"
+
+import resolvers from "./resolvers.js";
 
 const server =  new ApolloServer({
     typeDefs,  //typeDefs: typeDefs

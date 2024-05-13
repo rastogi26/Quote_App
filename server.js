500aeab6d883ff48096b4b00a292b07ec162e024
dhruv <dr26dhruv@gmail.com>
2024-05-13 13:00:16 +0530

    spearation of files

--- server.js
+++ server.js
@@ -1,44 +1,7 @@
 import { ApolloServer,gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import {users,quotes} from "./fakeDB.js"
-
-const typeDefs = gql`
-
-type Query{
-    users: [User]
-    user(id:ID!): User   # api to find user by id type User
-    quotes: [Quote]
-    iquote(by:ID!): [Quote] #array of quotes
-}
-
-type User{
-    id:ID!
-    fname: String
-    lname:String
-    email:String
-    password: String
-    quotes: [Quote]    #user can have arrays of quote Reference Quote
-}
-
-type Quote{
-    content: String
-    by:ID
-}
-
-
-`
-
-const resolvers = {
-    Query:{
-        users:()=>users,
-        user:(_,{id})=>users.find(user=>user.id==id),                // first argument is parent but it is already a root so we use _ and second is id
-        quotes:()=>quotes,
-        iquote:(_,{by})=>quotes.filter(quote=>quote.by==by)
-    },
-    User:{
-        quotes:(ur)=>quotes.filter(quote=>quote.by==ur.id)   //parent 
-    }
-}
+import typeDefs from './SchemaGQL.js'
+import resolvers from './resolvers.js'
 
 
 const server =  new ApolloServer({

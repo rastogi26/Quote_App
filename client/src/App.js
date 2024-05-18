7e01f2422f001557144599e1c127724705caad9f
dhruv <dr26dhruv@gmail.com>
2024-05-18 22:08:49 +0530

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import CreateQuote from './components/CreateQuote';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Signup from './components/SignUp';
+
+
+function App() {
+  return (
+    <div>
+      {/* <Login /> */}
+      {/* <Signup/> */}
+      {/* <Profile/> */}
+      {/* <CreateQuote/> */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;

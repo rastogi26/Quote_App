0265bbeef285c5bb545b499738aebdd3a488dd91
dhruv <dr26dhruv@gmail.com>
2024-05-19 13:09:36 +0530

    query all the quotes in home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,31 @@
+import { useQuery } from '@apollo/client';
 import React from 'react'
+import { GET_ALL_QUOTES } from '../gqlOperations/queries';
 
 export default function Home() {
+         
+  const {loading,error,data} = useQuery(GET_ALL_QUOTES)
+  
+   if (loading)return <h1>Loading</h1>
+   if (error) {
+    console.log(error.message);
+   }
+   if (data.quotes.length ===0 ) {
+    return <h1>No quotes available</h1>
+   }
+
   return (
     <div className="container">
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-        <p className='right-align'>~ram</p>
-      </blockquote>
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-        <p className='right-align'>~sham</p>
-      </blockquote>
+    {
+      data.quotes.map(quote=>{
+        return (
+          <blockquote>
+            <h6>{quote.name }</h6>
+            <p className="right-align">~{quote.by.fname}</p>
+          </blockquote>
+        );
+      })
+    }
     </div>
   );
 }

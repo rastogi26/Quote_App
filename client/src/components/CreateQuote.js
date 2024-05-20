c356579763f6dd014bbe781d9c289c1611471d31
dhruv <dr26dhruv@gmail.com>
2024-05-20 14:43:31 +0530

    createQuote

--- client/src/components/CreateQuote.js
+++ client/src/components/CreateQuote.js
@@ -1,13 +1,44 @@
+import { useMutation } from '@apollo/client'
 import React, { useState } from 'react'
+import { CREATE_QUOTE } from '../gqlOperations/mutations'
+
 
 export default function CreateQuote() {
     const [quote,setQuote]=useState("")
+    const [createQuote, { loading, error, data }] = useMutation(CREATE_QUOTE, {
+      refetchQueries: ["getAllQuotes", "getMyProfile"], //refresh every time  getAllQuotes when new quote created so that apoloclient dont not take from cache
+    });
+
+
     const handleSubmit= (e)=>{
         e.preventDefault()
-        console.log(quote);
+        // console.log(quote);
+        createQuote({
+          variables:{
+            name:quote
+          }
+        })
+    }
+    if (loading) {
+      return (
+        <div className="progress">
+          <div className="indeterminate">
+            <h2>Loading</h2>
+          </div>
+        </div>
+      );
+    }
+
+    // if (error) {
+    //   console.log(error.message); 
+    // }
+    if (data) {
+      console.log(data);
     }
   return (
     <div className="container my-container">
+      {error && <div className="red card-panel">{error.message}</div>}
+      {data && <div className="green card-panel">{data.quote}</div>}
       <form onSubmit={handleSubmit}>
         <input
           type="text"

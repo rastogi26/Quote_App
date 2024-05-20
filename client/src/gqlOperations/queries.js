51e9ccfc25d3260ac33706df03e6af26129f8ad0
dhruv <dr26dhruv@gmail.com>
2024-05-20 19:20:01 +0530

    queries and routes

--- client/src/gqlOperations/queries.js
+++ client/src/gqlOperations/queries.js
@@ -10,3 +10,30 @@ export const GET_ALL_QUOTES = gql`
     }
   }
 `;
+
+export const GET_MY_PROFILE=gql`
+query getMyProfile{
+  user:myprofile{
+     fname
+     lname
+     email
+     quotes{
+      name
+     }
+  }
+}
+`
+
+export const GET_USER_BY_ID = gql`
+  query getUserById($userid: ID!) {
+    user(_id: $userid) {
+      _id
+      fname
+      lname
+      email
+      quotes {
+        name
+      }
+    }
+  }
+`;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import CreateQuote from "./components/CreateQuote";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import OtherUserProfile from "./components/OtherUserProfile";
 import Profile from "./components/Profile";
 import Signup from "./components/SignUp";
 
@@ -10,4 +11,5 @@ export const routes = [
     { path: "/signup",element:<Signup/> },
     { path: "/profile",element:<Profile/> },
     { path: "/create",element:<CreateQuote/> },
+    { path: "/profile/:userid",element:<OtherUserProfile/> },
 ];

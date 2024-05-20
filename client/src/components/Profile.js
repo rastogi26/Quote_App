663e13a63e4ac88bad3e2c84116058eb1dfddb42
dhruv <dr26dhruv@gmail.com>
2024-05-20 18:56:22 +0530

    user profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,22 +1,46 @@
+import { useQuery } from '@apollo/client';
 import React from 'react'
+import { GET_MY_PROFILE } from '../gqlOperations/queries';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile() {
+  const {loading,error,data} = useQuery(GET_MY_PROFILE)
+  const navigate = useNavigate()
+
+  if (!localStorage.getItem("token")) {
+    navigate("/login")
+    return <h1>Unauthorized</h1>
+  }
+  if (loading) {
+    return (
+      <div className="progress">
+        <div className="indeterminate">
+          <h2>Loading</h2>
+        </div>
+      </div>
+    );
+  }
+  if (error) {
+    console.log(error);
+  }
   return (
     <div className="container my-container">
       <div className='center-align'>
-        <img className="circle" style={{border: "2px solid", marginTop:"10px"}} src="https://robohash.org/ram.png?size=200x200" alt="pic" />
-        <h5>Rajesh Kumar</h5>
-        <h6>Email:- raj@gmail.com</h6>
+        <img className="circle" style={{border: "2px solid", marginTop:"10px"}} src={`https://robohash.org/${data.user.fname}.png?size=200x200`} alt="pic" />
+        <h5>{data.user.fname} {data.user.lname}</h5>
+        <h6>Email:- {data.user.email}</h6>
       </div>
       <h3>Your Quotes</h3>
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-  
-      </blockquote>
-      <blockquote>
-        <h6>if it works dont touch it</h6>
-       
-      </blockquote>
+      {
+        data.user.quotes.map(quo=>{
+          return (
+            <blockquote>
+              <h6>{quo.name}</h6>
+            </blockquote>
+          );
+        })
+      }
+
     </div>
   );
 }
